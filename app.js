bf35cf16311e64e265b82c5fa4f6764a3081cbc9
supermelody90 <supermelody90@gmail.com>
2017-08-18 22:52:46 +1000

    first time to render homepage

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', routes);
+
+app.use(function(req, res, next) {
+    var err = new Error('not found');
+    err.status = 404;
+    next(err);
+});
