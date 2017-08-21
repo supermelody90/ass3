b7ab0d404bdc645241604a644b15773847c464a4
supermelody90 <supermelody90@gmail.com>
2017-08-21 16:33:28 +1000

    improve code of views and app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//import the packages
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+//import the url routing models
 var routes = require('./routes/index');
 var news = require('./routes/news');
 var forum = require('./routes/forum');
@@ -10,14 +12,16 @@ var topicCreate = require('./routes/topicCreate');
 
 var app = express();
 
+//the global data for reading data from file
 app.locals.filedata = require('./data.json');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//use the middleware and set the url routing
 app.use(logger('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname + '/public'));
 app.use('/', routes);
 app.use('/news', news);
@@ -30,5 +34,4 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// module.exports = app;
 app.listen(8080);
