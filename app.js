3bdfcfc2126bed8725ca5ef78dbb5b474badf6c0
supermelody90 <supermelody90@gmail.com>
2017-10-15 16:56:52 +1100

    modify

--- app.js
+++ app.js
@@ -76,4 +76,9 @@ app.use(function(req, res, next) {
     next(err);
 });
 
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
 module.exports = app;
